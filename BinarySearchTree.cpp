4b9d2835ef245fcf31e004ff188d5fc18305171e
Indra2512 <mononeon1225@gmail.com>
2025-06-26 10:28:05 +0700

    menambahkan fungsi search

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -75,8 +75,22 @@ class BinaryTree
                 // 7b. exit
                 return;
             }
-            
+        }
 
+        void search(int element, Node *&parent, Node *&currentNode)
+        {
+            // this function searches the currentNode of the specified Node as well as the current Node of its parent
+            currentNode = ROOT;
+            parent = nullptr;
+            while ((currentNode != nullptr) && (currentNode->info != element))
+            {
+                parent = currentNode;
+                if (element < currentNode->info)
+                    currentNode = currentNode->leftchild;
+                else
+
+            }
+            
         }
 
 };
